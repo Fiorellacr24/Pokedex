63befecd759ad9fb4c63730f934bc92f87ce391c
Fiorellacr24 <fiorella.0624@gmail.com>
2017-06-15 18:03:41 -0500

    close modal

--- public/js/components/modal.js
+++ public/js/components/modal.js
@@ -3,11 +3,11 @@
 const Modal = () => {
     const modal = $('<div id="modal1" class="modal"></div>');
     const contmodal = $('<div class="modal-content"></div>');
-    const close = $('<div class="modal-close">x</div>');
+    const close = $('<span class="float-right modal-close">x</span>');
     const h4 = $('<h4>Holi</h4>');
 
-    contmodal.append(h4);
     contmodal.append(close);
+    contmodal.append(h4);
     modal.append(contmodal);
 
     close.on('click', () => {
